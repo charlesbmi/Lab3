323e8c5da3a2df4f9c9a8f4ffce4e0d34ff0cec3
Nipun Agarwala <nipuna1@ee108-3b.stanford.edu>
2014-02-23 17:25:56 -0800

    Forwarding and Stalling Logic added

--- src/decode.v
+++ src/decode.v
@@ -11,6 +11,11 @@ module decode (
     input [31:0] instr,
     input [31:0] rs_data_in,
     input [31:0] rt_data_in,
+    input [4:0] forwarded_reg_addr_mem,
+    input [31:0] forwarded_data_mem,
+    input [4:0] forwarded_reg_addr_alu,
+    input [31:0] forwarded_data_alu,
+    input [5:0] op_prev,
 
     output wire [4:0] reg_write_addr,
     output wire jump_branch,
@@ -27,9 +32,8 @@ module decode (
     output wire [4:0] rs_addr,
     output wire [4:0] rt_addr,
 
-    output wire stall//, // TODO: put more signal inputs here that will be
-                         // used for forwarding and stalling
-                         // (don't forget about the comma)
+    output wire stall,
+    output wire [5:0] op_out
 );
 
 //******************************************************************************
@@ -39,12 +43,17 @@ module decode (
     wire [5:0] op = instr[31:26];
     assign rs_addr = instr[25:21];
     assign rt_addr = instr[20:16];
+    assign op_out = op;
     wire [4:0] rd_addr = instr[15:11];
     wire [4:0] shamt = instr[10:6];
     wire [5:0] funct = instr[5:0];
     wire [15:0] immediate = instr[15:0];
 
     wire [31:0] rs_data, rt_data;
+    wire [31:0] alu_op_x_initial;
+    wire [31:0] alu_op_y_initial;
+    wire [31:0] alu_op_x_temp;
+    wire [31:0] alu_op_y_temp;
 
 //******************************************************************************
 // branch instructions decode
@@ -183,13 +192,13 @@ module decode (
     // otherwise use rs
 
     wire [31:0] shift_amount = isShiftImm ? shamt : rs_data[4:0];
-    assign alu_op_x = isShift ? shift_amount : rs_data;
+    assign alu_op_x = isShift ? shift_amount : alu_op_x_initial;
     		
     // for link operations, use next pc (current pc + 4)
     // for immediate operations, use Imm
     // otherwise use rt
     
-    assign alu_op_y = isLink ? pc + 4'h8 : (use_imm ? imm : rt_data);
+    assign alu_op_y = isLink ? pc + 4'h8 : (use_imm ? imm : alu_op_y_initial);
     assign reg_write_addr = isLink ? `RA : (use_imm ? rt_addr : rd_addr);
     
     // determine when to write back to a register (any operation that isn't a branch, jump, or store)
@@ -201,11 +210,24 @@ module decode (
     assign mem_we = (op == `SW);    // write to memory
     assign mem_read = (op == `LW);    // use memory data for writing to register
 
+
+//******************************************************************************
+// Forwarding Control
+//******************************************************************************
+
+assign alu_op_x_temp = (forwarded_reg_addr_mem == rs_addr)? forwarded_data_mem: rs_data;
+assign alu_op_y_temp = (forwarded_reg_addr_mem == rt_addr)? forwarded_data_mem: rt_data;
+
+assign alu_op_x_initial = (forwarded_reg_addr_alu == rs_addr)? forwarded_data_alu: alu_op_x_temp;
+assign alu_op_y_initial = (forwarded_reg_addr_alu == rt_addr)?forwarded_data_alu: alu_op_y_temp;
+
+assign stall = ((op_prev == `LW) & ((forwarded_reg_addr_alu == rs_addr) | (forwarded_reg_addr_alu == rt_addr)))? 1'b1: 1'b0;
+
 //******************************************************************************
 // Branch resolution
 //******************************************************************************
     
-    wire isEqual = rs_data == rt_data;
+    wire isEqual = alu_op_x_initial == alu_op_y_initial;
     wire isZero = ~|rs_data;
     wire isNeg = rs_data[31];
     wire isPos = ~(isZero | isNeg);
